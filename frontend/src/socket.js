c536371f83a211b628389cb777316db3db0cea4f
Bucky Yu <chenkaiy@andrew.cmu.edu>
2025-01-26 15:11:03 -0800

    Fix Error

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Connect to the WebSocket server
-SOCKET_URL = process.env.REACT_APP_SOCKET_BASE_URL || 'http://localhost:5001'
+const SOCKET_URL = process.env.REACT_APP_SOCKET_BASE_URL || 'http://localhost:5001'
 
 const socket = io(SOCKET_URL); // Make this ENV
 
